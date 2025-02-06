5145a7916d281496dfa04216a480e63cdcc4e2fe
Michael Barlage <michael.barlage@noaa.gov>
2025-02-06 19:23:41 +0000

    update ncar scripts for naming

--- run_case/run_mpas_case_B1NCAR.sh
+++ run_case/run_mpas_case_B1NCAR.sh
@@ -13,9 +13,9 @@ clean_before="true"
 # model options
 ################################################################
 
-model_base_directory="/lfs5/BMC/wrfruc/Michael.Barlage/mpas/testing/code/ncar/intel-mpi/v8.2.2/"
-model_executable="atmosphere_model.41e9a3fb8.debug"
-model_code_base="ncar-v8.2.2-debug"
+model_base_directory="/lfs5/BMC/wrfruc/Michael.Barlage/mpas/testing/code/ncar/MPAS-Model/"
+model_executable="atmosphere_model.v8.2.2.intdebug"
+model_code_base="ncar-v8.2.2-intdebug"
 physics_suite="convection_permitting"
 run_directory=""
 namelist_version="ncar"
@@ -24,7 +24,7 @@ namelist_version="ncar"
 # input file options
 ################################################################
 
-input_case_base="/lfs5/BMC/wrfruc/Michael.Barlage/mpas/baselines_mpas/create_case/ncar-41e9a3fb8-intelmpi/"
+input_case_base="/lfs5/BMC/wrfruc/Michael.Barlage/mpas/baselines_mpas/create_case/ncar-v8.2.2-intelmpi/"
 input_code_base="ncar"
 input_namelist="ncar"
 resolution="120km"

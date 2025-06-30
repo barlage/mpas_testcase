2c96e37035a7daa704681f203917240be2cc618d
Michael Barlage <michael.barlage@noaa.gov>
2025-06-30 19:05:11 +0000

    add convection_permitting test

--- utils/compare_run_testcases_final.sh
+++ utils/compare_run_testcases_final.sh
@@ -19,9 +19,9 @@
 ##############################
 
 test_repo_name="gsl"                # repository name
-version_to_compare="v8.3.0-1.0"     # GSL version proposed for this PR
+version_to_compare="v8.3.0-1.2"     # GSL version proposed for this PR
 test_directory="/lfs5/BMC/wrfruc/Michael.Barlage/mpas/baseline_factory/mpas_testcase/run_case/"
-gsl_version_baseline="v8.2.3-3.12"   # GSL version of current develop
+gsl_version_baseline="v8.3.0-1.1"   # GSL version of current develop
 partition="xjet"                    # xjet or hera
 
 #####################################
@@ -280,6 +280,41 @@ echo >> $fileout
 
 dir1=$test_directory"$test_repo_name-$version_to_compare$compile_flag.mesoscale_reference.ncar.ncar.conus.120km.gfs.2023031015/"
 dir2=$ncar_baseline_directory"ncar-$ncar_version_baseline$compile_flag.mesoscale_reference.ncar.ncar.conus.120km.gfs.2023031015/"
+
+if [ ! -d $dir1 ]; then 
+  echo
+  echo "directory $dir1 does not exist"
+  exit
+fi
+
+if [ ! -d $dir2 ]; then 
+  echo
+  echo "directory $dir2 does not exist"
+  exit
+fi
+
+file="history.2023-03-10_15.00.00.nc"
+echo "  === $file comparison" >> $fileout
+nccmp -dsSqf $dir1$file $dir2$file >> $fileout
+echo >> $fileout
+
+file="history.2023-03-10_15.12.00.nc"
+echo "  === $file comparison" >> $fileout
+nccmp -dsSqf $dir1$file $dir2$file >> $fileout
+echo >> $fileout
+
+file="history.2023-03-10_16.00.00.nc"
+echo "  === $file comparison" >> $fileout
+nccmp -dsSqf $dir1$file $dir2$file >> $fileout
+echo >> $fileout
+
+echo "########################################################################" >> $fileout
+echo "# compare to previous NCAR CONUS convection_permitting baselines B1NCAR   " >> $fileout
+echo "########################################################################" >> $fileout
+echo >> $fileout
+
+dir1=$test_directory"$test_repo_name-$version_to_compare$compile_flag.convection_permitting.ncar.ncar.conus.120km.gfs.2023031015/"
+dir2=$ncar_baseline_directory"ncar-$ncar_version_baseline$compile_flag.convection_permitting.ncar.ncar.conus.120km.gfs.2023031015/"
 
 if [ ! -d $dir1 ]; then 
   echo
